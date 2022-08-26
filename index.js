5ce091941d0604aa8bc275a9aa5cb1bd2aa802c6
keith <nyakinyekeith@gmail.com>
2022-08-26 22:13:30 +0300

    complete

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide (){
+    let num1 = 2;
+    let num2 = 10;
+   return  num1 / num2;
+}
+
+let square = (num1) =>{
+    return num1 **2;
+
+}
+
+let add = (num1, num2) =>{
+    return num1 + num2;
+}
